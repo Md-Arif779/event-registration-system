3aefc89d93b62bd519e5e2e07a7d5e6878160f9d
Md-Arif779 <arifmahmudovi997@gmaile.com>
2023-12-30 15:50:48 +0600

    Add models, views, authentication, admin, search, user dashboard, and API

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location_name = models.CharField(max_length=255)
+    available_slots = models.PositiveIntegerField()
+
+class UserRegistration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
